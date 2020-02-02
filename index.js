9ae6982e956ebcf51f21a63e54dc9a55dab81190
Stackingrule <messageswitching@gmail.com>
2020-02-02 20:18:31 +0800

    update project

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const Koa = require('koa');
 
 const app = new Koa();
 
-app.use((ctx) => {
-    ctx.body = 'Hello World!'
+app.use(async (ctx, next) => {
+    await next();
+    console.log(1);
+    ctx.body = 'Hello World! API'
 });
+
+app.use(async (ctx) => {
+    console.log(2);
+})
 
 app.listen(3000);
