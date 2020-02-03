c88a5e647a37c7032bc54d6157e3f92225f74cfb
Stackingrule <messageswitching@gmail.com>
2020-02-03 21:04:12 +0800

    learn 发送 http

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 const router = new Router();
 const usersRouter = new Router({prefix: '/users'});
+
+const db = [{name: '李雷'}, {name: '韩梅梅'}];
 
 const auth = async (ctx, next) => {
     if (ctx.url !== '/users') {
@@ -12,30 +15,35 @@ const auth = async (ctx, next) => {
 };
 
 router.get('/', (ctx) => {
-    ctx.body = '这是主页';
+    ctx.body = '<h1>这是主页</h1>';
 });
 
 usersRouter.get('/', (ctx) => {
-    ctx.body = [{name: '李雷'}, {name: "韩梅梅"}];
+    // ctx.set('Allow', 'GET, POST');
+    // ctx.body = [{name: '李雷'}, {name: "韩梅梅"}];
+    ctx.body = db;
 });
 
 usersRouter.post('/', (ctx) => {
-    ctx.body = {name: '李雷'};
+    db.push(ctx.request.body);
+    ctx.body = ctx.request.body;
 });
 
 usersRouter.get('/:id', (ctx) => {
-    ctx.body = {name: '李雷'};
+    ctx.body = db[ctx.params.id * 1];
 });
 
 usersRouter.put('/:id', (ctx) => {
-    ctx.body = {name: '李雷2'};
+    db[ctx.params.id * 1] = ctx.request.body;
+    ctx.body = ctx.request.body;
 });
 
 usersRouter.delete('/:id', (ctx) => {
+    db.splice(ctx.params.id * 1, 1);
     ctx.status = 204;
 });
 
-
+app.use(bodyParser());
 app.use(router.routes());
 app.use(usersRouter.routes());
 app.use(usersRouter.allowedMethods());
