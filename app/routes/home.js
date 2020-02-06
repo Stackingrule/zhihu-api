f7627388c28192539aa02b06f5c38f618acf2296
Stackingrule <messageswitching@gmail.com>
2020-02-06 11:01:19 +0800

    add uploads

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,10 +1,11 @@
 const Router = require('koa-router');
 const router = new Router();
 // const homeCtl = require('../controllers/home');
-const { index } = require('../controllers/home');
+const { index, upload } = require('../controllers/home');
 
 // router.get('/', homeCtl.index);
 
 router.get('/', index);
+router.post('/upload', upload);
 
 module.exports = router;
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,5 +1,5 @@
 const Router = require('koa-router');
-const jsonwebtoken = require('jsonwebtoken');
+const jwt= require('koa-jwt');
 const router = new Router({prefix: '/users'});
 const {
         find, findById,
@@ -8,18 +8,7 @@ const {
         login, checkOwner
         } = require('../controllers/users');
 const { secret } = require('../config');
-const auth = async(ctx, next) => {
-        const { authorization = '' } = ctx.request.header;
-        const token = authorization.replace('Bearer', '');
-        try {
-                const user = jsonwebtoken.verify(token, secret);
-                ctx.state.user = user;
-        } catch (err) {
-                ctx.throw(401, err.message);
-        }
-        await next();
-};
-
+const auth = jwt({ secret });
 
 router.get('/', find);
 
