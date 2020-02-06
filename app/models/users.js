658c3fca3e24f605970797afd904a43ee452f2d2
Stackingrule <messageswitching@gmail.com>
2020-02-06 11:41:01 +0800

    add 个人资料 Schema

--- app/models/users.js
+++ app/models/users.js
@@ -6,6 +6,26 @@ const userSchema = new Schema({
     __v: { type: Number,select: false },
     name: { type: String, required: true },
     password: { type: String, required: true, select: false },
+    avatar_url: { type: String },
+    gender: { type: String, enum: ['male', 'female'], default: 'male', required: true },
+    headline: { type: String },
+    locations: { type: [{ type: String }] },
+    business: { type: String },
+    employments: {
+        type: [ {
+            company: {type: String},
+            job: {type: String}
+        } ],
+    },
+    eduactions: {
+        type: [ {
+            school: { type: String },
+            major: { type: String },
+            diploma: { type: Number, enum: [1, 2, 3, 4, 5] },
+            entrance_year: { type: Number },
+            graduation_year: { type: Number },
+        } ]
+    }
 });
 
 
