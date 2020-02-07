f2e4481af819f2aa843a5d397e78c5db19ba074f
Stackingrule <messageswitching@gmail.com>
2020-02-07 23:07:42 +0800

    add 分页

--- app/models/topics.js
+++ app/models/topics.js
@@ -6,8 +6,7 @@ const topicSchema = new Schema({
     __v: { type: Number,select: false },
     name: { type: String, select: true },
     avatar_url: { type: String },
-    introduction: { type: String, select: false },
+    introduction: { type: String }
 });
-
 
 module.exports = model('Topic', topicSchema);
